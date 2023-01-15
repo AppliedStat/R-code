7f8abc00c33d02ca05a8f52569b35d267777f1eb
statpnu <cpark2@gmail.com>
2023-01-15 22:23:40 +0900

    R codes

--- 2023b/Example-1.r
+++ 2023b/Example-1.r
@@ -2,22 +2,21 @@
 source("https://raw.githubusercontent.com/AppliedStat/R-code/master/2023b/Rcc22.R")
 
 #========================================================================
-# Data from Table 3.10 (Radiotherapy and Chemotherapy) of Lawless (2003). 
+# Data from Table 3.10 (Radiotherapy) of Lawless (2003). 
 # Statistical Models and Methods for Lifetime Data. Wiley, New York.
 # Lower ends in [a,b]
 X1 = c(
- 8, 0, 24, 17, 17, 24, 16, 13, 11, 16,
-18, 17, 32, 23, 44, 10, 0, 5, 12, 11,
-33, 31, 13, 19, 34, 13, 16, 35, 15, 11,
-22, 48, 30, 13, 10, 8, 4, 11, 14, 4,
-34, 30, 18, 16, 35, 21, 11 )
-# Upper ends in [a,b]
+45, 25, 37, 6, 46, 0, 0, 26, 18, 46,
+46, 24, 46, 27, 36, 7, 36, 5, 17, 46,
+19, 7, 36, 17, 37, 37, 24, 0, 40, 32,
+ 4, 17, 33, 15, 46, 19, 11, 11, 37, 22,
+38, 34, 46, 5, 36, 46, 14, 10)
 X2 = c(
- 12, 22, 31, 27, 23, 30, 24, Inf, 13, 20,
- 25, 26, Inf, Inf, 48, 35, 5, 8, 20, Inf,
- 40, Inf, 39, 32, Inf, Inf, 24, Inf, 22, 17,
- 32, Inf, 34, Inf, 17, 21, 9, Inf, 19, 8,
- Inf, 36, 24, 60, 39, Inf, 20)
+Inf,  37, Inf,  10, Inf,   5,   7, 40, Inf, Inf,
+Inf, Inf, Inf,  34, Inf,  16,  44, 11, Inf, Inf,
+ 35,  14,  48,  25,  44, Inf, Inf,  8, Inf, Inf,
+ 11,  25, Inf, Inf, Inf,  26,  15, 18, Inf, Inf,
+Inf, Inf, Inf,  12, Inf, Inf,  17, 35)
 XX = cbind(X1,X2)
 #========================================================================
 
@@ -31,6 +30,6 @@ weibull.ic.EM (XX, start=c(1,1))
 nlm(neg.loglike.weibull, p=c(1,1), X=XX)
 
 # The Newton-type method is successful with a certain starting value.
-nlm(neg.loglike.weibull, p=c(2,30), X=XX)
+nlm(neg.loglike.weibull, p=c(2,50), X=XX)
 
 
--- 2023b/Readme.txt
+++ 2023b/Readme.txt
@@ -6,7 +6,7 @@ A Note on Weibull Parameter Estimation with Interval Censoring Using the EM Algo
 
 # Illustrative Examples
 
-* Example-1.r 
+* Example-1.r: Table 3.10 (Radiotherapy) 
 
-* Example-2.r 
+* Example-2.r: Table 3.10 (Radiotherapy and Chemotherapy)
 
