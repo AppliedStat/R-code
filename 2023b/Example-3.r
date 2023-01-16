83abd4383a61888d81dba85f47a383a4dacf43a8
statpnu <cpark2@gmail.com>
2023-01-16 12:19:47 +0900

    R codes

---
+++ 2023b/Example-3.r
@@ -0,0 +1,27 @@
+
+source("https://raw.githubusercontent.com/AppliedStat/R-code/master/2023b/Rcc22.R")
+
+# Full observations
+
+#========================================================================
+# Data from Example 8.1 on Page 222 of Leemis (2009). Reliability 2nd ed.
+Bearings = c(17.88, 28.92, 33.00, 41.52, 42.12, 45.60, 48.48, 51.84, 51.96,
+             54.12, 55.56, 67.80, 68.64, 68.64, 68.88, 84.12, 93.12, 98.64, 
+            105.12,105.84,127.92,128.04,173.40)
+XX = cbind(Bearing,Bearing)
+#========================================================================
+
+
+# The EM method can find the global maximizer.
+weibull.ic.EM (XX, start=c(1,1))
+
+weibull.ic.EM (XX, start=c(5,5))
+
+
+
+# The Newton-type estimates depend on a starting value.
+nlm(neg.loglike.weibull, p=c(1,1), X=XX)
+
+nlm(neg.loglike.weibull, p=c(5,5), X=XX)
+
+
--- 2023b/Readme.txt
+++ 2023b/Readme.txt
@@ -7,6 +7,11 @@ A Note on Weibull Parameter Estimation with Interval Censoring Using the EM Algo
 # Illustrative Examples
 
 * Example-1.r: Table 3.10 (Radiotherapy) 
+  Lawless (2003). Statistical Models and Methods for Lifetime Data. Wiley, New York.
 
 * Example-2.r: Table 3.10 (Radiotherapy and Chemotherapy)
+  Lawless (2003). Statistical Models and Methods for Lifetime Data. Wiley, New York.
+
+* Example-3.r: Ball Bearing Data from Example 8.1 on Page 222 
+  Leemis (2009). Reliability 2nd ed.
 
