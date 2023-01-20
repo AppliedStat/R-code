1edb8b3ba179915a27533b4c56071acdf8dfac84
statpnu <cpark2@gmail.com>
2023-01-20 12:06:52 +0900

    R codes

--- 2023c/Wanomaly.R
+++ 2023c/Wanomaly.R
@@ -37,49 +37,6 @@ normal.3sigma.mean = function(data, factor=3) {
 #=====================================================================
 
 
-#------------------------------
-weibull.3sigma.HL = function(data, factor=3, a=0.5) {    # Breakdown point is only 29%
-   data = sort(data)
-   x = log(data);
-   y = log(-log(1-ppoints(x, a=a)))
-   DX = outer(x,x,"-"); DY = outer(y,y,"-")
-   diag(DX) = NA
-   idx = upper.tri(DY)
-   alpha.hat = median( (DY[idx]/DX[idx]) )  # slope 
-   intercept = median(y-alpha.hat*x) ## This intercept is better than the below. 
-   ## intercept = median( apply((outer(x,y,"*")-outer(y,x,"*"))/DX, 2, median, na.rm=TRUE) )
-   pred = intercept + alpha.hat*x
-   error = y - pred
-
-   sigma = shamos(error)
-   idx = abs(error)/sigma < factor
-   return( list(pure=data[idx], outlier=data[!idx]) )
-}
-
-
-#------------------------------
-weibull.3sigma.med = function(data, factor=3, a=0.5) {    # Breakdown point is only 29%
-   data = sort(data)
-   x = log(data);
-   y = log(-log(1-ppoints(x, a=a)))
-
-   DX = outer(x,x,"-"); DY = outer(y,y,"-")
-   diag(DX) = NA
-   Sxx = median( apply(DX^2, 2, median, na.rm=TRUE) )   # robust version of Sxx
-   Sxy = median( apply(DX*DY,2, median, na.rm=TRUE) )   # robust version of Sxy
-   alpha.hat = Sxy / Sxx # slope
-   intercept = median(y-alpha.hat*x) ## This intercept is better than the below. 
-   ## intercept = median( apply((outer(x,y,"*")-outer(y,x,"*"))/DX, 2, median, na.rm=TRUE) )
-
-   pred = intercept + alpha.hat*x
-   error = y - pred
-
-   sigma = mad(error)
-   idx = abs(error)/sigma < factor
-   return( list(pure=data[idx], outlier=data[!idx]) )
-}
-
-
 #=====================================================================
 # Generalized Mean Square Error
 gMSE = function(x,y, mux, muy) {
@@ -91,38 +48,8 @@ gMSE = function(x,y, mux, muy) {
    det(S)
 }
 #------------------------------
-gVAR = function(x,y) {
-   det( var(cbind(x,y)) )
-}   
+gVAR = function(x,y) { det( var(cbind(x,y)) ) }   
 #=====================================================================
-
-
-
-#=====================================================================
-# Shamos estimator
-shamos = function (x, constant = 1.048358, na.rm = FALSE, IncludeEqual = FALSE) 
-{
-    if (na.rm) 
-        x <- x[!is.na(x)]
-    w1 = outer(x, x, "-")
-    w2 = abs(w1[lower.tri(w1, diag = IncludeEqual)])
-    constant * median(w2)
-}
-#=====================================================================
-# Hodges-Lehmann estimator
-HL = function (x, estimator = c("HL1", "HL2", "HL3"), na.rm = FALSE) 
-{
-    estimator = match.arg(estimator)
-    if (na.rm) 
-        x <- x[!is.na(x)]
-    xx = outer(x, x, "+")
-    HL.estimation = switch(estimator, HL1 = 0.5 * median(xx[lower.tri(xx, 
-        diag = FALSE)]), HL2 = 0.5 * median(xx[lower.tri(xx, 
-        diag = TRUE)]), HL3 = 0.5 * median(xx))
-    return(HL.estimation)
-}
-#=====================================================================
-
 
 
 #=====================================================================
@@ -134,12 +61,5 @@ rweibull.mix = function(n, shapes, scales, prob) {
     rweibull(n=n, shape=shapes[components], scale=scales[components])
 }
 #=====================================================================
-# n = 50
-# x = rweibull.mix (n, shapes=c(2,5), scales=c(1,5), prob=c(0.9, 0.1)) 
-
-
-
-
-
 
 
