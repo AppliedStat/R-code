4fdbbb5d287c9d7615dc8409291208b1fc0f7b7a
statpnu <cpark2@gmail.com>
2023-01-20 11:49:58 +0900

    R codes

--- 2023c/Example-2.r
+++ 2023c/Example-2.r
@@ -6,7 +6,7 @@ source("https://raw.githubusercontent.com/AppliedStat/R-code/master/2023c/Wanoma
 library(weibullness)
 
 # ===========================================================
-# Data set (Thymic Lymphoma) from
+# Data set (Reticulum Cell Sarcoma) from
 # -----------------------------------------------------------
 # 1. Hoel, D. (1972). A representation of mortality data by competing risks. 
 #    Biometrics 28, 475–488.
@@ -14,8 +14,9 @@ library(weibullness)
 # 2. Kalbfleisch, J. and R. Prentice (2002). The statistical analysis of failure time data.
 #    Wiley, New York (See Data Set V in Appendix A).
 # -----------------------------------------------------------
-Data = c(159, 189, 191, 198, 200, 207, 220, 235, 245, 250, 256, 
-         261, 265, 266, 280, 343, 356, 383, 403, 414, 428, 432 )
+Data = c(317, 318, 399, 495, 525, 536, 549, 552, 554, 337, 558, 571, 586, 
+         594, 596, 605, 612, 621, 628, 631, 636, 643, 647, 648, 649, 661, 
+         663, 666, 670, 695, 697, 700, 705, 712, 713, 738, 748, 753)
 # -----------------------------------------------------------
 
 # Weibullness test with the original data
--- 2023c/Readme.txt
+++ 2023c/Readme.txt
@@ -4,5 +4,10 @@ Anomaly Detection Based on Cross Validation Under Weibull Distribution
 
 
 # Illustrative Examples
+  Data set from Table 1 of Hoel, D. (1972). 
+  A representation of mortality data by competing risks. Biometrics 28, 475–488.
 
-  Example-1.r 
+* Example-1.r (Thymic Lymphoma)
+
+* Example-2.r (Reticulum Cell Sarcoma)
+
